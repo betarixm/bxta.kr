c14ac6749a8dd4d320b3a0bc46686445b72c66e4
beta <beta.underalpha@gmail.com>
2022-03-28 16:45:28 +0900

    (#2) Resume: Add PGP public key

--- src/contents/resume/index.tsx
+++ src/contents/resume/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { MarkGithubIcon, MailIcon } from "@primer/octicons-react";
+import { MarkGithubIcon, MailIcon, KeyIcon } from "@primer/octicons-react";
+import { Link } from "gatsby";
 
 export interface Experience {
     title: string;
@@ -204,4 +205,66 @@ export const Contacts = [
         indicator: <MailIcon />,
         content: "beta.underalpha@postech.ac.kr",
     },
+    {
+        indicator: <KeyIcon />,
+        content: <Link to={"/pgp"}>PGP Public Key</Link>,
+    },
 ];
+
+export const Pgp: string =
+    "-----BEGIN PGP PUBLIC KEY BLOCK-----\n" +
+    "Version: Mailvelope v4.5.1\n" +
+    "Comment: https://www.mailvelope.com\n" +
+    "\n" +
+    "xsFNBGJBX8cBEADWhcWe4zT6iexlvKqbwXWU6C9qlq4bbP74x0xFivVplntB\n" +
+    "RjtdPacDaqNwzg5ahlg4AELo+8XtGBVBdvgPnawxJ1ktAl7ukwoEcDT9rnuB\n" +
+    "FeqP3Na2d2hi/hTrxsYm7s0kDdZpjx+H1XfYHr3yp3yWRmlYIlV/JR/hhuCS\n" +
+    "KvzlhcTV1KvvL3tqeo5UH3kDAMLIwu9qnOvA20/LXRLN7OG60LXiupHnW8vj\n" +
+    "uagCNd7BlPpDo3CHowXzdThkpxN4X31ysoDgDUm97jCMBi8zx0q7+TfWUCRj\n" +
+    "30DlK9OzmBHr+v9iRpp+p9IpXCAatrAwSkmCQMHWsq7QwH1XJsP8NE3WrpIJ\n" +
+    "7fj0JvI1wgSo18VD5vvah+It11ScZE67iEHhkNMza5aMEp1xYykyiz1NoHCF\n" +
+    "125sYyy9C9oH6oWNk7dOWT01tcikfz6L8msYgCcR6sKVqiYa4EIQDeBYBcJu\n" +
+    "er8J1Kb4k4QpNKHHsZ6A1ITwNZ4KcVPEzZfgCMt+3tp81JRLAfu8mPBppQA+\n" +
+    "AmNTtVEgFfFNpXAkmSYveH3I04Ek9F6M4Gfp0rctSUU1a5vmyf9QyGwuHVT2\n" +
+    "VALxxcs9YYPxpX+Fyw2SOpyptVmLQJGGjhrRlNkoaa0joXhJPYnqCktkThmX\n" +
+    "uCShwNvOo3I2bqFagIo2O2AHjffotWXwvMx4WwARAQABzSdHd29uIE1pbmph\n" +
+    "ZSA8YmV0YS51bmRlcmFscGhhQGdtYWlsLmNvbT7CwXUEEAEIAB8FAmJBX8cG\n" +
+    "CwkHCAMCBBUICgIDFgIBAhkBAhsDAh4BAAoJEGWq/0X/CjwTNYIP/16/D48c\n" +
+    "w3AzG3hntJQ3oQU+nLKuqYzAG56MrwSyBJ3eN6ZKbFR5m0zEYNGNZCpOLtsj\n" +
+    "vI+BdJZX8X6R4h0WHZC05iTBSmQyD+AgbeGDbb4SAcyoPGe0dQTrZ2GO20md\n" +
+    "p0aFE4GRtqwRNkUK451DG+9deA9t8XwSXE9NQ2DoKHCa+5axQvEvGFqqq1Y8\n" +
+    "v0l/D93j37caQ49bDWQg6rZ4PLkahXJoG+Zh59wAMqFSu9STwTdtLm4A/ZMR\n" +
+    "pYcx9kolGffrz1Yhy4EqC/UUooGIQ9w5Zuxqdf5ZCyw9GgljjepSi1wg6W3n\n" +
+    "CioGaJ1TxpRk2J+Qy5m2fl2pGQuiOEVHXGM+USqp3YpaRLZQDbYzpJD2u2sz\n" +
+    "CA2qWoEt7HENeEDp+5Rzje8yQSP/xDEFmjyOpcplcbLyMIxkJQS35O0RwkW6\n" +
+    "8kHxC5/mwjyMca6xFpg3goV46G98YDpcAkQhxvNMI3HfQ67DusTrPV0WpH+Q\n" +
+    "wMCPAHjsdhIqFqMdX5lWspTFoBrHAFM8nb4V/Z9yJ/6K933z9dEjEJJLzYj0\n" +
+    "dKlfN91+osXMBFzlRBDerCNtJM1vKwrJ39w/YNXmGBl3Z2KAoUYguOMqXt/c\n" +
+    "6UJJEcRIkTbxk2lo0MMC1suTDdfe56ZLHONyxInyKE3iN12SQfvxWHNmyEeM\n" +
+    "2xRwB1CXP3ltznf9zsFNBGJBX8cBEAC2ydO4ls484KGH+ow5s2/TgZBlS5Cy\n" +
+    "nO1wCSk/ng21heeEXhHZfvs3my6Nf3zIzYGvgAifDgEc8m2GjGnAD+PFPXhe\n" +
+    "3kx4QPKIFP3a/mHLn1CgwTGkH/sX4uhSijCuK0Lf3/OcG4AeXuk0wusx8xPF\n" +
+    "IRdw3dVKowzvaiPrRy6qsbRnLLAqCK438BW/PHns1AhPinlFr1RgkkB2wiF3\n" +
+    "I7+l8B0fTHe/5CkUCkvEbgbjSnC/yPizDNp+uuZIm9nBqKwif/a7+qr9MSFp\n" +
+    "VeVSDvxLdqbYZiDIzxIZghfW8C5BHWRI6vUVGqCY4w16BAPf8ucfy+bWdDV+\n" +
+    "cEQnoqrDT2ayWEkO8NWQhMq2KDmUROexR4FVavz/GxQkHVMzcidZEFdYpEFa\n" +
+    "+o+XTgo8a+BGTsJyapeukRyLXsJalcELxoAXRzx7sM+9vgfQrsD9SAg0Iock\n" +
+    "SitGDYpDDwBL88ZGqEfLAhWxkw/xhthbctJZGBHiGcGy5wG2tdvL965yUMWK\n" +
+    "8xCObztUICIMX/IxP8b8VPa0q5FjPxFI21eiLtj+9DVlPBWjaKnQocVhJqhb\n" +
+    "kjhz/SjDFbQxI3C6hM4cpizxj1YZMIV4bizfKfKKAobQsaHmTu1ts6uHwRMZ\n" +
+    "TTH6Yxvzq141AE0FRxBlq1cXlxPUhszaMUDXbmXqzg6xrIlDvsff0QARAQAB\n" +
+    "wsFfBBgBCAAJBQJiQV/HAhsMAAoJEGWq/0X/CjwT138P/2zVhMKPRy9pGnBr\n" +
+    "PfOaYxwohSWm2jZx/otgKZC129D4Sau+gvfspuZx17m39/CdL9XehcqXE/TC\n" +
+    "0LuggcvVU4PzQCr6HnG3DTh0/+VJICJTM/ESUZxORRbJeQPK0JPVocCbUrsV\n" +
+    "2GloCDW09fMwMcyPNGhUk+5OAeia557te7uM9uQrHHbyxSZ4k6zLptRT+m69\n" +
+    "UGpYgAx+rW4fMRFLXGb5JVz9W1pY64Vv1Z/UrYvnFlpum/sPw9Pw6a1xOPu9\n" +
+    "yCqfRKy54gIcrfeCumQLe7QhE7kGxQpsynOqYFqm1mBp8wCmi4+SWs8Bs+UI\n" +
+    "SaZd71O+xTqq8N6y+Bue3JuuZtjLXcr/GVT3ozR54CbhG9VcrmmgGFg7IlA/\n" +
+    "Jav5d4RJYZwtTGvtIln2tyz4Vrqybvz/WCkphUbCC84nsAm3KEmVX90lqoDV\n" +
+    "/Dp3N+u7zAa1xngnPjkAFb5iN54XCp//sjTMw/W2ubobbA5MB8OPL8tlNmdb\n" +
+    "/HnFun2Z9ZDmmeeVumAKJw9isgrXW5v0E28dsjWn4CeQzH1JgX+fRFRHvcFI\n" +
+    "C+ImX6aSquwdC53G62NW5M+TjBjhWLu2qrhq2sYhluO227zaNlS7KrCJdT/W\n" +
+    "ydxg0GquxmLLc7v5kJV82JXS+KlEOFcqZoeWjE6l+9aGoITVO1Wp6cGK1spn\n" +
+    "SosXnVjW\n" +
+    "=mpHi\n" +
+    "-----END PGP PUBLIC KEY BLOCK-----";
---
+++ src/pages/pgp.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Layout from "../components/Layout";
+import { Pgp as PgpPublicKey } from "../contents/resume";
+
+interface PubProps {}
+
+interface PubState {}
+
+class Pgp extends React.Component<PubProps, PubState> {
+    render = () => {
+        return (
+            <Layout>
+                <pre style={{ fontFamily: "monospace" }}>{PgpPublicKey}</pre>
+            </Layout>
+        );
+    };
+}
+
+export default Pgp;
